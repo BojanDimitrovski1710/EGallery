e347941657cf54a134a0eceab244ac039a1ab22d
BojanDimitrovski1710 <70719546+BojanDimitrovski1710@users.noreply.github.com>
2022-02-16 15:18:46 +0100

    Add files via upload

---
+++ EGalleryApplication/EGallery.Web/Controllers/ProductsController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+using EGallery.Domain.Identity;
+using EGallery.Repository;
+using EGallery.Domain.DomainModels;
+using EGallery.Domain.DTO;
+using EGallery.Services.Interface;
+
+namespace EGallery.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly UserManager<EGalleryApplicationUser> _userManager;
+
+        public ProductsController(IProductService productService, UserManager<EGalleryApplicationUser> userManager)
+        {
+            _productService = productService;
+            _userManager = userManager;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var allProducts = _productService.GetAllProducts();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            ProductDTO prodDT = new ProductDTO
+            {
+                loggedInUser = user,
+                Products = allProducts
+            };
+            return View(prodDT);
+        }
+
+        public IActionResult View(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = this._productService.GetDetailsForProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Name,Image,Description,Price,Rating")] Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                this._productService.CreateNewProduct(product);
+                return RedirectToAction("Index", "Products");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public IActionResult Edit(Guid? p)
+        {
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            var product = this._productService.GetDetailsForProduct(p);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        public IActionResult AddToShoppingCart(Guid? id)
+        {
+            var model = this._productService.GetShoppingCartInfo(id);
+            
+            ViewBag.image = model.SelectedProduct.Image;
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddToShoppingCart([Bind("ProductId", "Quantity", "SelectedProduct")]AddToShoppingCartDto item)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = this._productService.AddToShoppingCart(item, userId);
+
+            if (result)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            return View(item);
+        }
+
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Guid id, [Bind("Id,Name,Image,Description,Price,Rating")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    this._productService.UpdeteExistingProduct(product);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Products");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public IActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = this._productService.GetDetailsForProduct(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+       
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(Guid id)
+        {
+            this._productService.DeleteProduct(id);
+            return RedirectToAction("Index" , "Products");
+        }
+
+        private bool ProductExists(Guid id)
+        {
+            return this._productService.GetDetailsForProduct(id) != null;
+        }
+    }
+}
---
+++ EGalleryApplication/EGallery.Web/Controllers/ShoppingCartController.cs
@@ -0,0 +1,68 @@
+﻿using EGallery.Domain.DomainModels;
+using EGallery.Domain.DTO;
+using EGallery.Domain.Identity;
+using EGallery.Repository;
+using EGallery.Services.Implementation;
+using EGallery.Services.Interface;
+using GemBox.Document;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EGallery.Web.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        
+        private readonly IShoppingCartService _shoppingCartService;
+        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+        public IActionResult Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return View(this._shoppingCartService.getShoppingCartInfo(userId));
+        }
+
+        public IActionResult DeleteProductFromShoppingCart(Guid productId)
+        {
+            //delete
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = this._shoppingCartService.deleteProductFromShoppingCart(userId, productId);
+
+            return RedirectToAction("Index", "ShoppingCart");
+        }
+
+        public IActionResult ViewProduct (Guid productId)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = this._shoppingCartService.getProduct(userId, productId);
+
+            return View(result);
+        }
+
+        public IActionResult SubmitOrder()
+        {
+
+            return RedirectToAction("ShowInvoice", "ShoppingCart");
+        }
+
+        public FileContentResult ShowInvoice(MemoryStream stream)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = this._shoppingCartService.orderNow(userId);
+
+            return File(result.ToArray(), new PdfSaveOptions().ContentType, "ExportInvoice.pdf");
+        }
+    }
+}
